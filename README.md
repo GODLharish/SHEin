0d20b0a872b6e98ae7d59227363cd6b15adbbcfc
GODLharish <soulharish07@gmail.com>
2026-03-29 19:50:44 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SHEin
+
+![SHEin Demo](SHEin_demo.mp4)
+
+This project contains the frontend clone of Two Good Co rebranded as SHEin.
---
+++ push.ps1
@@ -0,0 +1,9 @@
+git init
+git lfs install
+git lfs track "*.mp4"
+git add .gitattributes
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/GODLharish/SHEin.git
+git push -u origin main
