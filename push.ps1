cda0855bec642afeda1dbf6e67a298a734e86fcb
GODLharish <106647926+GODLharish@users.noreply.github.com>
2026-03-29 19:53:09 +0530

    Delete push.ps1

--- push.ps1
+++
@@ -1,9 +0,0 @@
-git init
-git lfs install
-git lfs track "*.mp4"
-git add .gitattributes
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/GODLharish/SHEin.git
-git push -u origin main
